a6459be407e3d4b6354007735638fb89bc0e592c
Kazuhiro Kobayashi <kkobayashi.oa@gmail.com>
2024-03-23 19:29:13 +0900

    refactor: remove unnecessary fetching in EpisodeDetailPage

--- workspaces/app/src/pages/EpisodeDetailPage/index.tsx
+++ workspaces/app/src/pages/EpisodeDetailPage/index.tsx
@@ -5,7 +5,6 @@ import invariant from 'tiny-invariant';
 
 import { useBook } from '../../features/book/hooks/useBook';
 import { EpisodeListItem } from '../../features/episode/components/EpisodeListItem';
-import { useEpisode } from '../../features/episode/hooks/useEpisode';
 import { Box } from '../../foundation/components/Box';
 import { Flex } from '../../foundation/components/Flex';
 import { Separator } from '../../foundation/components/Separator';
@@ -19,12 +18,11 @@ const EpisodeDetailPage: React.FC = () => {
   invariant(episodeId);
 
   const { data: book } = useBook({ params: { bookId } });
-  const { data: episode } = useEpisode({ params: { episodeId } });
 
   return (
     <Box>
       <section aria-label="漫画ビューアー">
-        <ComicViewer episodeId={episode.id} />
+        <ComicViewer episodeId={episodeId} />
       </section>
 
       <Separator />
@@ -32,7 +30,15 @@ const EpisodeDetailPage: React.FC = () => {
       <Box aria-label="エピソード一覧" as="section" px={Space * 2}>
         <Flex align="center" as="ul" direction="column" justify="center">
           {book.episodes.map((episode) => (
-            <EpisodeListItem key={episode.id} bookId={bookId} episodeId={episode.id} />
+            <EpisodeListItem
+              key={episode.id}
+              bookId={bookId}
+              chapter={episode.chapter}
+              description={episode.description}
+              episodeId={episode.id}
+              imageId={episode.imageId}
+              name={episode.name}
+            />
           ))}
         </Flex>
       </Box>
