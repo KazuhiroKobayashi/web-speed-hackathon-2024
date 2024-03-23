5aa57293c39928d68915639cba541f7c5dd2df6a
Kazuhiro Kobayashi <kkobayashi.oa@gmail.com>
2024-03-23 15:03:09 +0900

    chore: remove jquery

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,4 +1,3 @@
-import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
@@ -9,24 +8,25 @@ import { ClientApp } from '@wsh-2024/app/src/index';
 import { preloadImages } from './utils/preloadImages';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  if (window.location.pathname.startsWith('/admin')) {
+    ReactDOM.createRoot(root!).render(<AdminApp />);
+  } else {
+    ReactDOM.hydrateRoot(
+      root!,
+      <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
+        <BrowserRouter>
+          <ClientApp />
+        </BrowserRouter>
+      </SWRConfig>,
+    );
+  }
+});
+
 const main = async () => {
   await registerServiceWorker();
   // await preloadImages();
-
-  $(document).ready(() => {
-    if (window.location.pathname.startsWith('/admin')) {
-      ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
-    } else {
-      ReactDOM.hydrateRoot(
-        $('#root').get(0)!,
-        <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
-          <BrowserRouter>
-            <ClientApp />
-          </BrowserRouter>
-        </SWRConfig>,
-      );
-    }
-  });
 };
 
 main().catch(console.error);
