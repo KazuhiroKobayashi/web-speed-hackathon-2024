967de3bc8b16afee2d6fcaafa7ed4eb9c2713304
Kazuhiro Kobayashi <kkobayashi.oa@gmail.com>
2024-03-24 10:38:32 +0900

    refactor: more simplify hero

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -1,20 +1,10 @@
 import styled from 'styled-components';
 
-const _Wrapper = styled.div`
-  aspect-ratio: 16 / 9;
-  width: 100%;
-`;
-
 const _Image = styled.img`
   width: 100%;
-  height: 100%;
-  object-fit: fill;
+  aspect-ratio: 16 / 9;
 `;
 
 export const HeroImage: React.FC = () => {
-  return (
-    <_Wrapper>
-      <_Image alt="Cyber TOON" src="/assets/hero.avif" />
-    </_Wrapper>
-  );
+  return <_Image alt="Cyber TOON" src="/assets/hero.avif" />;
 };
