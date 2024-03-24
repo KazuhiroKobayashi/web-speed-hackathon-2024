167dd34da39ea0a7036373a2da6ee21d8a84921c
Kazuhiro Kobayashi <kkobayashi.oa@gmail.com>
2024-03-24 12:08:10 +0900

    fix: rollback p-queue

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,6 +1,13 @@
 /// <reference types="@types/serviceworker" />
+import PQueue from 'p-queue';
+
 import { transformJpegXLToBmp } from './transformJpegXLToBmp';
 import { zstdFetch as fetch } from './zstdFetch';
+
+// ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
+const queue = new PQueue({
+  concurrency: 5,
+});
 
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
@@ -11,7 +18,11 @@ self.addEventListener('activate', (ev: ExtendableEvent) => {
 });
 
 self.addEventListener('fetch', (ev: FetchEvent) => {
-  ev.respondWith(onFetch(ev.request));
+  ev.respondWith(
+    queue.add(() => onFetch(ev.request), {
+      throwOnTimeout: true,
+    }),
+  );
 });
 
 async function onFetch(request: Request): Promise<Response> {
